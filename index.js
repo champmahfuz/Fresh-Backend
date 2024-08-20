e03a6c7a55e4c1fe50e0893e97cd5f1692466126
Mahfuz Rahman <mahfuzrahman517@gmail.com>
2024-08-20 11:47:04 +0600

    check problem

--- index.js
+++ index.js
@@ -10,20 +10,13 @@ const app = express()
 
 app.use(express.json());
 app.use(cookieParser())
-
 app.use(cors({
-    origin: 'https://fresh-front-end.vercel.app', // Replace with your frontend URL
+    origin: ['https://fresh-front-end.vercel.app/'], // Replace with your frontend URL
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization']
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Add other methods if needed
+    allowedHeaders: ['Content-Type', 'Authorization'] // Adjust headers as necessary
 }));
 
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin', 'https://fresh-front-end.vercel.app');
-//     res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
-//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-//     next();
-// });
 app.use('/api', router)
 
 app.options('/api/category-product', cors());
