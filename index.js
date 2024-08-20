c745ffeb08444b9356685035d4e67b42a059b9c2
Mahfuz Rahman <mahfuzrahman517@gmail.com>
2024-08-20 12:05:14 +0600

    added no cors

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express()
 app.use(express.json());
 app.use(cookieParser())
 
-app.use(cors());
+app.use(cors('*'));
 
 /* app.use(cors({
     origin: 'https://fresh-front-end.vercel.app', // Replace with your frontend URL
