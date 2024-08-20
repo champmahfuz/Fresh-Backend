34f02425903d48ba685a276ea856fd0eef8601fc
Mahfuz Rahman <mahfuzrahman517@gmail.com>
2024-08-20 11:59:36 +0600

    remove cors

--- index.js
+++ index.js
@@ -11,12 +11,14 @@ const app = express()
 app.use(express.json());
 app.use(cookieParser())
 
-app.use(cors({
+app.use(cors());
+
+/* app.use(cors({
     origin: 'https://fresh-front-end.vercel.app', // Replace with your frontend URL
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     allowedHeaders: ['Content-Type', 'Authorization']
-}));
+})); */
 
 app.use('/api', router)
 
